3802c3d4dccaddf01f0cf5df5fd41ca9948dbdb6
Emiliano Rotta <emilianorottaf@gmail.com>
2024-09-03 22:11:03 -0300

    ejercicios realizados

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,9 +15,9 @@
 //     ],
 //     saludar: function(){console.log(`Hola ${this.nombre}`)}
 // }
- 
+
 // alumno.saludar()
-// modulo 3 sesion 2 ejercicio de comprobacion
+
 
 //Ejercicio individual: Hasta las 20.10
 //Orden: 
@@ -42,17 +42,17 @@
 // Ejercicio 2: Uso de this en un método con función flecha
 // Consigna: Crea un objeto usuario con las propiedades nombre y edad, y un método imprimirEdad que utilice una función flecha dentro de un setTimeout para imprimir la edad después de 5 segundos.
 
-var usuario = {
-    nombre: "Juan",
-    edad: 20,
-    imprimirEdad: function() {
-        setTimeout(()=>{
-            console.log(`La edad de ${this.nombre} es ${this.edad}`)
-        },5000)     
-   
-    }
-}
-usuario.imprimirEdad()
+// var usuario = {
+//     nombre: "Juan",
+//     edad: 20,
+//     imprimirEdad: function() {
+//         setTimeout(()=>{
+//             console.log(`La edad de ${this.nombre} es ${this.edad}`)
+//         },5000)     
+
+//     }
+// }
+// usuario.imprimirEdad()
 
 // Ejercicio 3: Uso de this para modificar propiedades
 // Consigna: Crea un objeto contador con una propiedad valor inicializada en 0. Añade un método incrementar que incremente valor en 1 cada vez que se llame. Usa this para referenciar la propiedad valor 
@@ -67,3 +67,50 @@ usuario.imprimirEdad()
 // contador.incrementar()
 // contador.incrementar()
 // contador.incrementar()
+
+
+//-------------class--------------------
+//EcmaScript ES6 -- JS 2015
+
+// class Persona {
+//     constructor(nombre, edad) {
+//         this.nombre = nombre;
+//         this.edad = edad;
+//     }
+//     saludar() {
+//         console.log(`Hola ${this.nombre} tenes ${this.edad} años!`)
+//     }
+// }
+
+// const erick = new Persona("Erick", 25)
+// const lorena = new Persona("Lorena", 30)
+// erick.saludar()
+// lorena.saludar()
+
+
+class Auto {
+    constructor(marca, modelo, anio) {
+        this.marca = marca
+        this.modelo = modelo
+        this.anio = anio
+
+    }
+    detalles() {
+        console.log(`Auto: ${this.marca} ${this.modelo}, Año:${this.anio}`)
+    }
+}
+
+const autoActual = new Auto ("Renault", "Clio", 2007)
+const autoFuturo = new Auto ("Toyota", "Corolla", 2018)
+autoActual.detalles()
+autoFuturo.detalles()
+
+// Ejercicio 2: Añadir métodos a una clase
+// Consigna: Crea una clase CuentaBancaria con las propiedades titular y saldo. Añade métodos depositar y retirar que permitan modificar el saldo de la cuenta. Añade un método consultarSaldo que imprima el saldo actual. Crea una instancia de CuentaBancaria y realiza algunas operaciones.
+
+
+
+const miCuenta = new CuentaBancaria('Pedro', 1000);
+miCuenta.depositar(500); // Has depositado 500. Saldo actual: 1500
+miCuenta.retirar(200); // Has retirado 200. Saldo actual: 1300
+miCuenta.consultarSaldo(); // Saldo de Pedro: 1300
